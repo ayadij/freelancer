550a8d3a00854463563ec1f79708a166ad71cbda
ayadij <ayadijkwel@yahoo.com>
2018-03-19 12:35:02 -0700

    bootstrap cards added for document page. specific and global css files added

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
                 updated_at: '11/11/16',
-                image_url: 'http://google.com',
+                image_url: 'https://uangteman.com/blog/wp-content/uploads/2016/08/Yuk-Hitung-Untung-Rugi-Kerja-Sampingan-copy.jpg',
             },
             {
                 title: "My Second Doc",
                 description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
                 updated_at: '11/11/16',
-                image_url: 'http://google.com',
+                image_url: 'https://www.breadandbuttermarketing.com.au/wp-content/uploads/2016/04/Freelancer.jpg',
             },
             {
                 title: "My Last Doc",
                 description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
                 updated_at: '11/11/16',
-                image_url: 'http://google.com',
+                image_url: 'http://szssplit.hr/wp-content/uploads/2017/12/freelancing-tips.jpg',
             }
         ];
     }
@@ -38,7 +38,8 @@ var DocumentsComponent = /** @class */ (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
-            templateUrl: 'documents.component.html'
+            templateUrl: 'documents.component.html',
+            styleUrls: ['documents.component.css']
         })
     ], DocumentsComponent);
     return DocumentsComponent;
--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -4,32 +4,33 @@ import { Document } from './document';
 @Component({
   moduleId: module.id,
   selector: 'documents',
-  templateUrl: 'documents.component.html'
+  templateUrl: 'documents.component.html',
+  styleUrls: ['documents.component.css']
 })
 export class DocumentsComponent {
   pageTitle: string = "Document Dashboard"
-  
+
   documents: Document[] = [
     {
       title: "My First Doc",
       description: 'asdfasdfasdf asdfasd',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com',
+      image_url: 'https://uangteman.com/blog/wp-content/uploads/2016/08/Yuk-Hitung-Untung-Rugi-Kerja-Sampingan-copy.jpg',
     },
     {
       title: "My Second Doc",
       description: 'asdfasdfasdf asdfasd',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com',
+      image_url: 'https://www.breadandbuttermarketing.com.au/wp-content/uploads/2016/04/Freelancer.jpg',
     },
     {
       title: "My Last Doc",
       description: 'asdfasdfasdf asdfasd',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com',
+      image_url: 'http://szssplit.hr/wp-content/uploads/2017/12/freelancing-tips.jpg',
     }
   ]
 }
